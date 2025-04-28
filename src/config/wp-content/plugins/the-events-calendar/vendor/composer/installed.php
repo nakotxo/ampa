4535a12508a8137076b829bffe6685350726b9f5
Ignacio <ihidalgo@deustosistemas.net>
2025-04-28 09:34:17 +0200

    subida proyecto ampa

---
+++ src/config/wp-content/plugins/the-events-calendar/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/the-events-calendar',
+        'pretty_version' => 'dev-release/T25.daredevil.1',
+        'version' => 'dev-release/T25.daredevil.1',
+        'reference' => '6d064f3c5e9fda41c39ecef9718a37bba8265775',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/the-events-calendar' => array(
+            'pretty_version' => 'dev-release/T25.daredevil.1',
+            'version' => 'dev-release/T25.daredevil.1',
+            'reference' => '6d064f3c5e9fda41c39ecef9718a37bba8265775',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
